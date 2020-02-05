7bf6935f6d1c356557e60500747c4eb99e25b609
John Rudd <john.rudd@bglgroup.co.uk>
2020-02-05 18:40:59 +0000

    feat(changes): upgraded bootstrap, refactored service layer tests

---
+++ AddressBook.ServiceLayer.Tests/TestHelpers/QueryableExtensions.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * 
+ * Code taken from Microsoft examples. 
+ */
+using Moq;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace AddressBook.ServiceLayer.Tests.Overview.Queries
+{
+    public static class QueryableExtensions
+    {
+        public static DbSet<T> BuildMockDbSet<T>(this IQueryable<T> source)
+            where T : class
+        {
+            var mock = new Mock<DbSet<T>>();
+            mock.As<IDbAsyncEnumerable<T>>()
+                .Setup(x => x.GetAsyncEnumerator())
+                .Returns(new TestDbAsyncEnumerator<T>(source.GetEnumerator()));
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.Provider)
+                .Returns(new TestDbAsyncQueryProvider<T>(source.Provider));
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.Expression)
+                .Returns(source.Expression);
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.ElementType)
+                .Returns(source.ElementType);
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.GetEnumerator())
+                .Returns(source.GetEnumerator());
+
+            return mock.Object;
+        }
+    }
+}
---
+++ AddressBook.ServiceLayer.Tests/TestHelpers/TestDbAsyncEnumerator.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * 
+ * Code taken from Microsoft examples. 
+ */
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AddressBook.ServiceLayer.Tests.Overview.Queries
+{
+    internal class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public TestDbAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        object IDbAsyncEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
