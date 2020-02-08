b0cd2c75f146d7bdeda332321957869ea354d381
John Rudd <john.rudd@bglgroup.co.uk>
2020-02-08 07:47:49 +0000

    chore(sass): moved over site styles to sass - precompile steps

---
+++ AddressBook/gulpfile.js
@@ -0,0 +1,18 @@
+﻿
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+gulp.task('styles', () => {
+    return gulp.src('Styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./content/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ]);
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
